535858af863c83ed35dbc5239c4df394fb32adbe
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-25 10:01:51 +0300

    sign up works with axios , no feedback

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -1,33 +1,68 @@
+import axios from 'axios';
 import { useState } from 'react';
 
 import classes from './AuthForm.module.css';
+import { apiKey } from './../../config';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    // prijungti esama vartotoja
+    if (isLogin) {
+      console.log('login action');
+      return;
+    }
+    // sukurti vartotoja
+    if (!isLogin) {
+      const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + apiKey;
+      console.log('sign up action');
+      console.log('sending form', {
+        email,
+        password,
+        returnSecureToken: true,
+      });
+      // galima validacija,  pvz passwordai ar sutampa ir t.t.
+      const response = await axios.post(url, {
+        email,
+        password,
+        returnSecureToken: true,
+      });
+      console.log({ response });
+      return;
+    }
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input onChange={(e) => setEmail(e.target.value)} value={email} type='email' id='email' required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type='password'
+            id='password'
+            minLength='6'
+            required
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
+          <button type='button' className={classes.toggle} onClick={switchAuthModeHandler}>
             {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
         </div>
